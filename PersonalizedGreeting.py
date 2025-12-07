63f1cd29f11358197948cea09f10d6eb95d41030
Anamika Sinha <anamika13sinha@gmail.com>
2025-12-07 10:00:00 +0530

    Adding Personalized Greeting function

---
+++ PersonalizedGreeting.py
@@ -0,0 +1,4 @@
+firstName = input("Enter your first name: ").strip()
+lastname = input("Enter your last name: ").strip()
+
+print("Hello, " + firstName + " " + lastname + "! Welcome to the Python world.")
