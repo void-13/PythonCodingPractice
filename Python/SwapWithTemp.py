c9f7716202f2adf73a10383abf857fa901a13d54
Anamika Sinha <anamika13sinha@gmail.com>
2025-12-08 10:00:00 +0530

    Adding swap numbers with temp variable

---
+++ Python/SwapWithTemp.py
@@ -0,0 +1,9 @@
+first_number = input("Enter first number: ")
+second_number = input("Enter second number: ")
+
+temp = first_number
+first_number = second_number
+second_number = temp
+
+print("first number: " + first_number)
+print("second number: " + second_number)
