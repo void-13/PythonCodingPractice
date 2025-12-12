ce739fa9cfb4c6e5ba3d4492729e615742c4394a
Anamika Sinha <anamika13sinha@gmail.com>
2025-12-12 21:35:46 +0530

    Adding two numbers without using temp variable

---
+++ Python/SwapWithoutTemp.py
@@ -0,0 +1,7 @@
+first_number = input("Enter first number: ")
+second_number = input("Enter second number: ")
+
+first_number, second_number = second_number, first_number
+
+print("first number: " + first_number)
+print("second number: " + second_number)
