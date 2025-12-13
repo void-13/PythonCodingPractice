3a57d3c1c2b7068c4b5da71b65ec4a7cbbd7a09f
Anamika Sinha <anamika13sinha@gmail.com>
2025-12-13 20:27:30 +0530

    Python program that demonstrates logical operators

---
+++ Python/LogicalOperator.py
@@ -0,0 +1,15 @@
+def logical_operator():
+    x = True
+    y = False
+
+    and_result = x and y
+    or_result = x or y
+    not_result = not x
+
+    print(f"x = {x}, y = {y}")
+    print(f"x and y: {and_result}")
+    print(f"x or y: {or_result}")
+    print(f"x not y: {not_result}")
+
+
+logical_operator()
