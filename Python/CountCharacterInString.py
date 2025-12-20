7096527a359752c34d9db65ba75a31ced87503d8
Anamika Sinha <anamika13sinha@gmail.com>
2025-12-20 23:35:17 +0530

    Count number of characters in a string

---
+++ Python/CountCharacterInString.py
@@ -0,0 +1,13 @@
+input_string = input("Enter the input string: ")
+
+
+def countChar():
+    stringChar = input_string[::]
+    print("Total character count: ", len(stringChar))
+
+
+def main():
+    countChar()
+
+
+main()
